46b2fbad5ab9f29edef684c9f684cc7888e2d7d4
ncolosso <colossonico@gmail.com>
2023-07-10 16:32:43 -0400

    Domain name changes

--- frontend/src/config/config.prod.ts
+++ frontend/src/config/config.prod.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: "http://splitter.us-east-1.elasticbeanstalk.com/api/v1/splitter/"
+    apiUrl: "http://backend.splitterproject.com/api/v1/splitter/"
 }
 
 export default config;
