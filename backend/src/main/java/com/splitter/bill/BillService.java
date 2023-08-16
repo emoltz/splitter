1db72529d5ddd16ee256fb776c239dc20e6a1fbb
ncolosso <colossonico@gmail.com>
2023-08-16 12:30:08 -0700

    Basic backend logging for BillService

--- backend/src/main/java/com/splitter/bill/BillService.java
+++ backend/src/main/java/com/splitter/bill/BillService.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,6 +19,8 @@ import java.util.Optional;
 
 @Service
 public class BillService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BillService.class);
 
     private final BillRepository billRepository;
     private final FeeService feeService;
@@ -38,6 +43,7 @@ public class BillService {
     }
 
     public ResponseEntity<Bill> createBill(NewBillRequest newBillRequest) {
+        LOGGER.info("New Bill Request {}", newBillRequest);
         Bill bill = new Bill();
         bill.setTitle(newBillRequest.title());
         try {
@@ -50,6 +56,7 @@ public class BillService {
         try {
             billRepository.save(bill);
         } catch (Exception e) {
+            LOGGER.error("Could not create New Bill", e);
             return new ResponseEntity<>(null, HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
@@ -57,6 +64,7 @@ public class BillService {
         feeService.createFee(new NewFeeRequest("Tax", 0.0, bill.getId()));
         feeService.createFee(new NewFeeRequest("Tip", 0.0, bill.getId()));
 
+        LOGGER.info("New Bill created {}", bill);
         return new ResponseEntity<>(bill, HttpStatus.CREATED);
     }
 
@@ -64,5 +72,6 @@ public class BillService {
         Bill bill = billRepository.findById(id).orElseThrow(() -> new IllegalStateException("Bill with id " + id + " does not exist"));
         bill.setArchived(true);
         billRepository.save(bill);
+        LOGGER.info("Bill {} archived successfully", bill);
     }
 }
